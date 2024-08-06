f2597288aa5f24b2eb327d6ef6d9208e7d06ce2c
Kqpa <76552592+Kqpa@users.noreply.github.com>
2024-08-06 15:44:55 -0400

    oops

--- script.js
+++ script.js
@@ -1,32 +1,11 @@
-// Select the input field and the output div
-const API = `https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=`;
-
-const queryInput = document.getElementById('queryInput');
-
-// Add an event listener to the input field
-queryInput.addEventListener('input', function () {
-    // Get the current value of the input field
-    const currentValue = queryInput.value;
-    // Reset the field,
-    try {
-        fetch(`${API}${currentValue}`).then((response) => {
-            response.json().then((data) => {
-                const queries = data[1];
-
-                document.querySelector("#queries").innerHTML = "";
-                queries.forEach(suggestion => {
-                    document.querySelector("#queries").innerHTML += `<li><a href="https://www.youtube.com/results?search_query=${encodeURIComponent(suggestion)}">${suggestion}</a></li>`
-                });
-            })
-        })
-    } catch (error) {
-        console.error(error.message);
-    }
+document.getElementById('searchForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var query = document.getElementById('searchInput').value;
+    var url = 'https://www.youtube.com/results?search_query=' + encodeURIComponent(query);
+    window.location.href = url;
 });
 
-
-function search() {
-    const query = document.getElementById('queryInput').value;
-    const url = 'https://www.youtube.com/results?search_query=' + encodeURIComponent(query);
-    window.location.href = url;
-}
+// Focus on the input box when the page loads
+window.onload = function() {
+    document.getElementById('searchInput').focus();
+};

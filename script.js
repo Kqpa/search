9d85d8fb31a307151a7681a47d5cd99b3e0076b4
Bluefield <notshigetora@gmail.com>
2024-08-06 19:36:28 +0200

    feat: improvise query fetch

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
-document.getElementById('searchForm').addEventListener('submit', function(event) {
+const APIUrl = `http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=`
+
+window.onload = async function () {
+
+    /* event listener */
+    document.querySelector("#searchInput").addEventListener('change', fetchQueries);
+
+    /* function */
+    async function fetchQueries() {
+        const request = await fetch(`${APIUrl}${document.querySelector("#searchInput").value}`).catch(err => console.log(`ERR: ${err}`));
+        const res = await request.json().catch(err => console.log(`ERR: ${err}`));
+        console.log(res);
+    }
+}
+
+
+
+document.querySelector('#searchForm').addEventListener('submit', function (event) {
     event.preventDefault();
-    var query = document.getElementById('searchInput').value;
-    var url = 'https://www.youtube.com/results?search_query=' + encodeURIComponent(query);
+    const query = document.querySelector('#searchInput').value;
+    const url = `https://www.youtube.com/results?search_query=${encodeURIComponent(query)}`;
     window.location.href = url;
 });
 
-// Focus on the input box when the page loads
-window.onload = function() {
-    document.getElementById('searchInput').focus();
-};
+
